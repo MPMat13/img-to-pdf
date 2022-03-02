0128d37d43d59c474aa3981c0dd8f39f3446b812
MPMat13 <94404968+MPMat13@users.noreply.github.com>
2022-03-02 13:27:48 +0100

    Create mulitple_img2pdf.py

---
+++ mulitple_img2pdf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+#Python3 program to convert imgage to pdf.
+#im2pdf v b0.01
+
+import img2pdf
+from PIL import Image
+import os
+import time
+
+#path to image dir.
+img_dir = input('Path to image dir: ')
+
+# variable created for time count. can be activated.
+# t = 0
+
+# creating empty list
+img_lst = []
+
+# loop for localize every jpg img inside dir
+for i in os.listdir(img_dir):
+	if i.endswith('.jpg'):
+		# t += 1
+		img_lst.append(img_dir + '/' +i)
+		print(f'Added file {i} to page list.')
+
+# sorting file list, can be removed if u dont need pages in order
+print('sorting list...')
+img_lst.sort()
+# time.sleep(t/100)
+print('sorting finished...')
+
+# creating PDF file.
+print('\n\ncreating file...\n\n')
+with open(img_dir + '/file.pdf', 'wb') as f:
+	f.write(img2pdf.convert(img_lst))
+
+print('file created.')
